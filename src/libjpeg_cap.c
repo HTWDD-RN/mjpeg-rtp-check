07d02189dae41b9afe26665d2de5081e99611464
Emanuel Günther <s76954@informatik.htw-dresden.de>
2021-04-14 11:41:42 +0200

    Read some more information provided by libjpeg

--- src/libjpeg_cap.c
+++ src/libjpeg_cap.c
@@ -27,6 +27,12 @@ void process_jpeg(FILE** in) {
     if (cinfo.saw_JFIF_marker) {
         printf("square pixels?: %u\n", cinfo.X_density == cinfo.Y_density);
     }
+    /*
+    printf("max_h_samp_factor: %d\n", cinfo.max_h_samp_factor);
+    printf("max_v_samp_factor: %d\n", cinfo.max_v_samp_factor);
+    printf("min_DCT_h_scaled_size: %d\n", cinfo.min_DCT_h_scaled_size);
+    printf("min_DCT_v_scaled_size: %d\n", cinfo.min_DCT_v_scaled_size);
+    */
 
     row_stride = cinfo.output_width * cinfo.output_components;
     buffer = (*cinfo.mem->alloc_sarray)
@@ -35,15 +41,26 @@ void process_jpeg(FILE** in) {
     // read image
     while (cinfo.output_scanline < cinfo.output_height) {
         (void) jpeg_read_scanlines(&cinfo, buffer, 1);
+        // process some scan or frame specific data
     }
 
-    (void) jpeg_finish_decompress(&cinfo);
-
-    printf("SOSs: %d, prec: %d, baseline?: %u, huffman?: %u\n",
-            cinfo.input_scan_number, cinfo.data_precision, cinfo.is_baseline,
-            !cinfo.arith_code);
+    if (cinfo.quant_tbl_ptrs) {
+        printf("quant_tbl = 8bit?: %u\n", cinfo.quant_tbl_ptrs[0]->quantval[0] < 0xff);
+    }
+    printf("prec: %d\n", cinfo.data_precision);
+    printf("baseline?: %u, huffman?: %u\n", cinfo.is_baseline, !cinfo.arith_code);
     printf("progressive?: %u\n", cinfo.progressive_mode);
+    if (cinfo.cur_comp_info) {
+        for (size_t i = 0; i < cinfo.comps_in_scan; ++i) {
+            printf("c%zu: h: %d, v: %d\n", i,
+                    cinfo.cur_comp_info[i]->h_samp_factor,
+                    cinfo.cur_comp_info[i]->v_samp_factor);
+        }
+    }
+    printf("SOSs: %d\n", cinfo.input_scan_number);
     printf("DRIs: %u\n", cinfo.restart_interval);
+
+    (void) jpeg_finish_decompress(&cinfo);
 
     jpeg_destroy_decompress(&cinfo);
 }
